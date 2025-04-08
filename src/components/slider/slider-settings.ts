0a38f22847e0000271c78a77d6f5a5edf862e7d1
Geisiel <geisiel.nascimento@gmail.com>
2025-04-08 00:12:39 -0300

    feat: slide

---
+++ src/components/slider/slider-settings.ts
@@ -0,0 +1,12 @@
+export const sliderSettings = {
+  dots: false,
+  infinite: true,
+  speed: 800,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  draggable: true,
+  easing: 'easeInOutQuad',
+  fade: true
+};
