974ae532516b7201ea16489c1da90cfc01cb7e81
Geisiel <geisiel.nascimento@gmail.com>
2025-04-09 00:22:43 -0300

    feat: top loader

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import { notFound } from 'next/navigation'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import { routing } from '@/i18n/routing'
+import NextTopLoader from 'nextjs-toploader'
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
 const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] })
@@ -24,6 +25,7 @@ export default async function LocaleLayout({ children, params }: RootLayoutProps
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
+      <NextTopLoader color="#71717A" />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <NextIntlClientProvider locale={locale}>
             <Header />
